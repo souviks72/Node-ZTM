d821dfc1e198c48275d99e2102651b23519b126c
souviks72 <souvikmandalvit2016@gmail.com>
2023-01-11 21:36:37 +0530

    added post request to our basic server

--- 3.Web-Servers/index.js
+++ 3.Web-Servers/index.js
@@ -17,13 +17,32 @@ const friends = [
   },
 ];
 
+//req is a readable stream
+//res is a writeable stream
+//So we can connect the 2 streams via pipe
 const server = http.createServer((req, res) => {
   //if(req.method === "GET") {}
   const items = req.url.split("/");
   /*
+    URL Parameters::
     /friends/1  --> ['', friends, 1]
     */
-  if (items[1] === "friends") {
+  if (items.length < 2) {
+    res.statusCode = 400;
+    res.end();
+  }
+  if (req.method === "POST" && items[1] === "friends") {
+    //request body is a stream, so data is in bytes
+    req.on("data", (data) => {
+      const friend = data.toString();
+      console.log("Request: ", friend);
+      let friendObj = JSON.parse(friend);
+      friendObj = { ...friendObj, id: friends.length };
+      friends.push(friendObj);
+      console.log(friends);
+      req.pipe(res);
+    });
+  } else if ((req.method === "GET" && items[1]) === "friends") {
     res.writeHead(200, { "Content-Type": "application/json" });
     if (items.length == 2) res.end(JSON.stringify(friends));
     else res.end(JSON.stringify(friends[parseInt(items[2])]));
