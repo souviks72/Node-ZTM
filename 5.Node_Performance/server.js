20ed7caaf6e8b64a13e1b0da691da7b1e28f8d42
souviks72 <souvikmandalvit2016@gmail.com>
2023-02-20 19:09:44 +0530

    oauth security

---
+++ 5.Node_Performance/server.js
@@ -0,0 +1,38 @@
+//https://www.npmjs.com/package/pm2
+//https://cloud.google.com/architecture/application-deployment-and-testing-strategies
+const express = require("express");
+const cluster = require("cluster");
+const os = require("os");
+
+const app = express();
+
+function delay(duration) {
+  const startTime = Date.now();
+  while (Date.now() - startTime < duration) {
+    //event loop is blocked...
+  }
+}
+
+app.get("/", (req, res) => {
+  // JSON.stringify({}) => "{}"
+  // JSON.parse("{}") => {}
+  // [5,1,2,3,4].sort()
+  res.send(`Performance example: ${process.pid}`);
+});
+
+app.get("/timer", (req, res) => {
+  delay(9000);
+  res.send(`Ding ding ding! ${process.pid}`);
+});
+
+console.log("Running server.js...");
+if (cluster.isMaster) {
+  console.log("Master has been started...");
+  const NUM_WORKERS = os.cpus().length;
+  for (let i = 0; i < NUM_WORKERS; i++) {
+    cluster.fork();
+  }
+} else {
+  console.log("Worker process started.");
+  app.listen(3000);
+}
