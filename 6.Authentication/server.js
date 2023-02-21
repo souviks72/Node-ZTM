257accd179488f88dc1267f9e7198dd213e06ef6
souviks72 <souvikmandalvit2016@gmail.com>
2023-02-21 19:30:39 +0530

    using google oauth via passport

--- 6.Authentication/server.js
+++ 6.Authentication/server.js
@@ -3,6 +3,8 @@ const path = require("path");
 const https = require("https");
 const express = require("express");
 const helmet = require("helmet");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
 
 require("dotenv").config();
 
@@ -13,9 +15,26 @@ const config = {
   CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
 
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log(profile);
+  done(null, profile);
+  //if we used password auth, then accessToken would be the password and this function would
+  //be doing manual verification. However since we are using Google OAuth, we just need to pass
+  //params for now
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
 const app = express();
 
 app.use(helmet());
+app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true; //TODO
@@ -27,14 +46,34 @@ function checkLoggedIn(req, res, next) {
   next();
 }
 
-app.get("/auth/google", (req, res) => {});
+//first request to google auth server
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"], //we only want the email to be returned from google auth server, we can also ask for profile, name, etc
+  })
+);
 
-app.get("/auth/google/callback", (req, res) => {});
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("Google called us back!!");
+  }
+);
 
 app.get("/auth/logout", (req, res) => {});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your personal secret value is 42!");
+});
+
+app.get("/failure", (req, res) => {
+  res.send("Failed to login");
 });
 
 app.get("/", (req, res) => {
